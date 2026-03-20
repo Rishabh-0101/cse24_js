4a662687fd732d80f9d2a0144689428cc13d1ce8
Rishabh-0101 <rishabhpatel62799@gmail.com>
2026-03-20 19:17:42 +0530

    EXPERIMENT1.JS

---
+++ Assignment2/Experiment2.js
@@ -0,0 +1,32 @@
+// let employees = [];
+
+// function addEmployee(){
+
+//     let name = document.getElementById("name").value;
+//     let empId = document.getElementById("empId").value;
+//     let salary = parseFloat(document.getElementById("salary").value);
+//     let dept = document.getElementById("dept").value;
+//     if(name === " ")
+//         alert("please fill");
+//         return;
+
+
+// let employee = {
+//     name : name,
+//     id : empId,
+//     salary : salary,
+//     department : dept,
+
+// }
+
+// function displayEmployees(){
+//     let output = "<h3> all employees </h3>" ;
+//     employees.forEach((emp)=> {
+//         output += 
+//     })
+// }
+// }
+function myFunction(){
+    alert("Hello World!");
+}
+
